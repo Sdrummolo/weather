cdc87ba25f546e21bc23f873e312c0c36d48567f
Sdrummolo <52413900+Sdrummolo@users.noreply.github.com>
2020-05-05 10:10:01 +0100

    fixed indentation

--- static/application.js
+++ static/application.js
@@ -1,91 +1,91 @@
-const search = document.querySelector("input");
+const search = document.querySelector("input")
 // initialize google autocomplete
-const autocomplete = new google.maps.places.Autocomplete(search);
+const autocomplete = new google.maps.places.Autocomplete(search)
 // Initialize ui
-const ui = new UI();
+const ui = new UI()
 // API keys
-const DARKSKYAPIKEY = "a542b5ae979df6d58aab3d3e74f11164";
-const GOOGLEAPIKEY = "AIzaSyCMdvEyuNCCRTedIBxZKUf9pnvlL7bgk-g";
+const DARKSKYAPIKEY = "a542b5ae979df6d58aab3d3e74f11164"
+const GOOGLEAPIKEY = "AIzaSyCMdvEyuNCCRTedIBxZKUf9pnvlL7bgk-g"
 
 // Google proprietary event handler
-autocomplete.addListener("place_changed", getCoords);
+autocomplete.addListener("place_changed", getCoords)
 
 // Fetch user location weather
 if (navigator.geolocation) {
-	navigator.geolocation.getCurrentPosition(position => {
-		const lat = position.coords.latitude;
-		const lng = position.coords.longitude;
+   navigator.geolocation.getCurrentPosition((position) => {
+      const lat = position.coords.latitude
+      const lng = position.coords.longitude
 
-		getWeather(lat, lng);
-	});
+      getWeather(lat, lng)
+   })
 }
 
 // Get coordinates of searchbar input
 async function getCoords() {
-	let address = search.value;
+   let address = search.value
 
-	if (address != "") {
-		try {
-			let coordsRequest = await fetch(
-				`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${GOOGLEAPIKEY}`
-			);
-			let coordsData = await coordsRequest.json();
+   if (address != "") {
+      try {
+         let coordsRequest = await fetch(
+            `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${GOOGLEAPIKEY}`
+         )
+         let coordsData = await coordsRequest.json()
 
-			let lat = coordsData.results[0].geometry.location.lat;
-			let lng = coordsData.results[0].geometry.location.lng;
+         let lat = coordsData.results[0].geometry.location.lat
+         let lng = coordsData.results[0].geometry.location.lng
 
-			getWeather(lat, lng);
-		} catch (err) {
-			console.log(err);
-		}
-	}
+         getWeather(lat, lng)
+      } catch (err) {
+         console.log(err)
+      }
+   }
 }
 
 // Call weather API
 async function getWeather(lat, lng) {
-	// Fetch user's location data
-	try {
-		let locationRequest = await fetch(
-			`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${GOOGLEAPIKEY}`
-		);
-		// Fetch weather data
-		let weatherRequest = await fetch(
-			`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${DARKSKYAPIKEY}/${lat},${lng}?units=si`
-		);
+   // Fetch user's location data
+   try {
+      let locationRequest = await fetch(
+         `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${GOOGLEAPIKEY}`
+      )
+      // Fetch weather data
+      let weatherRequest = await fetch(
+         `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${DARKSKYAPIKEY}/${lat},${lng}?units=si`
+      )
 
-		let locationData = await locationRequest.json();
-		let weatherData = await weatherRequest.json();
+      let locationData = await locationRequest.json()
+      let weatherData = await weatherRequest.json()
 
-		changeBG(weatherData);
-		ui.showCurrentLocation(locationData);
-		ui.showCurrentWeather(weatherData);
-		ui.showForecast(weatherData);
-	} catch (err) {
-		console.log(err);
-	}
+      changeBG(weatherData)
+      ui.showCurrentLocation(locationData)
+      ui.showCurrentWeather(weatherData)
+      ui.showForecast(weatherData)
+   } catch (err) {
+      console.log(err)
+   }
 }
 
 // Determine sun state, day returns 2, nigh returns 1 and evening returns 3
 function changeBG(data) {
-	const currentTime = data.currently.time;
-	const sunriseTime = data.daily.data[0].sunriseTime;
-	const sunriseTimeTom = data.daily.data[1].sunriseTime;
-	const sunsetTime = data.daily.data[0].sunsetTime;
+   const currentTime = data.currently.time
+   const sunriseTime = data.daily.data[0].sunriseTime
+   const sunriseTimeTom = data.daily.data[1].sunriseTime
+   const sunsetTime = data.daily.data[0].sunsetTime
 
-	if (
-		currentTime >= sunriseTime &&
-		sunsetTime - currentTime <= 3600 &&
-		sunsetTime - currentTime > 0
-	) {
-		return ui.changeBgColor(3);
-	} else if (currentTime >= sunriseTime && sunsetTime - currentTime > 3600) {
-		return ui.changeBgColor(1);
-	} else if (
-		sunriseTimeTom - currentTime <= 3600 &&
-		sunriseTimeTom - currentTime > 0
-	) {
-		return ui.changeBgColor(3);
-	} else {
-		return ui.changeBgColor(2);
-	}
+   if (
+      currentTime >= sunriseTime &&
+      sunsetTime - currentTime <= 3600 &&
+      sunsetTime - currentTime > 0
+   ) {
+      return ui.changeBgColor(3)
+   } else if (currentTime >= sunriseTime && sunsetTime - currentTime > 3600) {
+      return ui.changeBgColor(1)
+   } else if (
+      sunriseTimeTom - currentTime <= 3600 &&
+      sunriseTimeTom - currentTime > 0
+   ) {
+      return ui.changeBgColor(3)
+   } else {
+      return ui.changeBgColor(2)
+   }
 }
--- static/ui.js
+++ static/ui.js
@@ -1,108 +1,106 @@
 class UI {
-	constructor() {
-		this.input = document.querySelector("input");
-		this.location = document.getElementById("loc");
-		this.date = document.getElementById("date");
-		this.temperature = document.getElementById("temperature");
-		this.conditions = document.getElementById("conditions");
-		this.high = document.getElementById("high");
-		this.humidity = document.getElementById("humidity");
-		this.rain = document.getElementById("rain");
-		this.wind = document.getElementById("wind");
-		this.low = document.getElementById("low");
-		this.feels = document.getElementById("feels");
-		this.body = document.querySelector("body");
-		this.day1 = document.getElementById("day-1");
-		this.day2 = document.getElementById("day-2");
-		this.day3 = document.getElementById("day-3");
-		this.day4 = document.getElementById("day-4");
-		this.day5 = document.getElementById("day-5");
-		this.day6 = document.getElementById("day-6");
-	}
+   constructor() {
+      this.input = document.querySelector("input")
+      this.location = document.getElementById("loc")
+      this.date = document.getElementById("date")
+      this.temperature = document.getElementById("temperature")
+      this.conditions = document.getElementById("conditions")
+      this.high = document.getElementById("high")
+      this.humidity = document.getElementById("humidity")
+      this.rain = document.getElementById("rain")
+      this.wind = document.getElementById("wind")
+      this.low = document.getElementById("low")
+      this.feels = document.getElementById("feels")
+      this.body = document.querySelector("body")
+      this.day1 = document.getElementById("day-1")
+      this.day2 = document.getElementById("day-2")
+      this.day3 = document.getElementById("day-3")
+      this.day4 = document.getElementById("day-4")
+      this.day5 = document.getElementById("day-5")
+      this.day6 = document.getElementById("day-6")
+   }
 
-	// Populate top part of UI with data
-	showCurrentWeather(data) {
-		this.date.textContent = new Date(
-			data.currently.time * 1000
-		).toDateString();
-		this.temperature.textContent =
-			Math.round(data.currently.temperature) + "°";
-		this.conditions.textContent = data.currently.summary;
-		this.high.textContent =
-			Math.round(data.daily.data[0].temperatureHigh) + "°";
-		this.rain.textContent =
-			Math.round(data.daily.data[0].precipProbability * 100) + "%";
-		this.wind.textContent = Math.round(data.currently.windSpeed) + "km/h";
-		this.low.textContent =
-			Math.round(data.daily.data[0].temperatureLow) + "°";
-		this.humidity.textContent =
-			Math.round(data.currently.humidity * 100) + "%";
-		this.feels.textContent =
-			Math.round(data.currently.apparentTemperature) + "°";
+   // Populate top part of UI with data
+   showCurrentWeather(data) {
+      this.date.textContent = new Date(
+         data.currently.time * 1000
+      ).toDateString()
+      this.temperature.textContent =
+         Math.round(data.currently.temperature) + "°"
+      this.conditions.textContent = data.currently.summary
+      this.high.textContent =
+         Math.round(data.daily.data[0].temperatureHigh) + "°"
+      this.rain.textContent =
+         Math.round(data.daily.data[0].precipProbability * 100) + "%"
+      this.wind.textContent = Math.round(data.currently.windSpeed) + "km/h"
+      this.low.textContent = Math.round(data.daily.data[0].temperatureLow) + "°"
+      this.humidity.textContent =
+         Math.round(data.currently.humidity * 100) + "%"
+      this.feels.textContent =
+         Math.round(data.currently.apparentTemperature) + "°"
 
-		// Show icon
-		const icon = new Skycons({ color: "#fff" });
-		icon.set("icon-main", data.currently.icon);
-		icon.play();
-	}
+      // Show icon
+      const icon = new Skycons({ color: "#fff" })
+      icon.set("icon-main", data.currently.icon)
+      icon.play()
+   }
 
-	// Takes an integer (1 for day, 2 for night, 3 for others) and changes bgcolor accordingly
-	changeBgColor(phase) {
-		if (phase === 1) {
-			this.body.style.background = `linear-gradient(
+   // Takes an integer (1 for day, 2 for night, 3 for others) and changes bgcolor accordingly
+   changeBgColor(phase) {
+      if (phase === 1) {
+         this.body.style.background = `linear-gradient(
 			   180deg,
 			   rgba(2, 0, 36, 1) 0%,
 			   rgba(102, 204, 253, 1) 0%,
 			   rgba(143, 214, 246, 1) 100%
-			)`;
-		} else if (phase === 2) {
-			this.body.style.background = `linear-gradient(
+			)`
+      } else if (phase === 2) {
+         this.body.style.background = `linear-gradient(
 				180deg, 
 				rgba(2, 0, 36, 1) 0%, 
 				rgba(64,102,167,1) 0%, 
 				rgba(202,167,166,1) 100%
-				)`;
-		} else if (phase === 3) {
-			this.body.style.background = `linear-gradient(
+				)`
+      } else if (phase === 3) {
+         this.body.style.background = `linear-gradient(
             180deg, rgba(2,0,36,1) 0%, 
             rgba(211,97,133,1) 0%, 
             rgba(249,208,103,1) 100%
-				)`;
-		}
-	}
+				)`
+      }
+   }
 
-	showForecast(data) {
-		let dates = [];
-		let icons = [];
-		let days = [
-			this.day1,
-			this.day2,
-			this.day3,
-			this.day4,
-			this.day5,
-			this.day6
-		];
+   showForecast(data) {
+      let dates = []
+      let icons = []
+      let days = [
+         this.day1,
+         this.day2,
+         this.day3,
+         this.day4,
+         this.day5,
+         this.day6,
+      ]
 
-		// Populate arrays
-		for (let i = 0; i < 7; i++) {
-			dates[i] = new Date(data.daily.data[i + 1].time * 1000);
-			icons[i] = new Skycons({ color: "#fff" });
-		}
+      // Populate arrays
+      for (let i = 0; i < 7; i++) {
+         dates[i] = new Date(data.daily.data[i + 1].time * 1000)
+         icons[i] = new Skycons({ color: "#fff" })
+      }
 
-		// Populate forecast
-		for (let [i, day] of days.entries()) {
-			day.innerHTML = `
+      // Populate forecast
+      for (let [i, day] of days.entries()) {
+         day.innerHTML = `
 			<h3>${dates[i].toDateString().split(" ")[0]}</h3>
 				<canvas id="icon-${i + 1}" width="70" height="70"></canvas>
 				<h3>${Math.round(data.daily.data[i + 1].temperatureHigh)}°</h3
-			`;
-			icons[i].set(`icon-${i + 1}`, data.daily.data[i + 1].icon);
-			icons[i].play();
-		}
-	}
+			`
+         icons[i].set(`icon-${i + 1}`, data.daily.data[i + 1].icon)
+         icons[i].play()
+      }
+   }
 
-	showCurrentLocation(data) {
-		console.log(data);
-		this.location.textContent = data.results[5].formatted_address;
-	}
+   showCurrentLocation(data) {
+      this.location.textContent = data.results[5].formatted_address
+   }
 }
